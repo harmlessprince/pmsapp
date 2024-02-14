eea21a0b28433a5b1b9aedfe78415d51520d752e
harmlessprince <realolamilekan@gmail.com>
2024-02-14 16:38:39 +0100

    fixed attendace route issue

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\Company\SiteController;
 use App\Http\Controllers\Company\TagController;
 use App\Http\Controllers\Company\UserController;
 use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\Attendancecontroller;
+use App\Http\Controllers\AttendanceController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ScanAnalyticsController;
 use App\Http\Controllers\ScanController;
@@ -36,7 +36,7 @@ Route::prefix('company')->middleware('auth')->name('company.')->group(function (
     Route::get('dashboard', [DashboardController::class, 'company'])->name('dashboard');
     Route::prefix('attendance')->name('attendance.')->group(function (){
         Route::get('analytics', AttendanceAnalyticsController::class)->name('analytics');
-        Route::get('transactions', [Attendancecontroller::class, 'index'])->name('transactions');
+        Route::get('transactions', [AttendanceController::class, 'index'])->name('transactions');
     });
     Route::prefix('scans')->name('scans.')->group(function (){
         Route::get('analytics', ScanAnalyticsController::class)->name('analytics');
